3005ec227c85b10581ebce8068964859a4238596
Naeem847 <ashoornaeem@gmail.com>
2026-07-11 18:18:52 +0500

    A machine learning data preprocessing project using Python, Pandas, and Scikit-learn. Includes data cleaning, missing value handling, feature scaling, label encoding, one-hot encoding, and dataset preparation for ML models.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.preprocessing import LabelEncoder
 
 df=pd.read_csv('D:\object_oriented_programming1.py\ML-Practice-code\data .csv')
 
@@ -20,3 +25,59 @@ df['Age']=df['Age'].fillna(df['Age'].median())
 
 print(df['Age'])
 
+df_cleaned=df.drop_duplicates()
+
+# cleaned the duplicates in the dataset
+
+# print the missing values in the dataset
+print(df.isnull().sum())
+
+df['Age'].fillna(df['Age'].mean(), inplace=True)
+
+df['Salary'].fillna(df['Salary'].mean(), inplace=True)
+
+print(df['Age'])
+
+print(df['Salary'])
+
+# initialize the scaler
+
+scaler = MinMaxScaler()
+
+# scale the features
+
+df[['Age', 'Salary']] = scaler.fit_transform(df[['Age', 'Salary']])
+
+# show normalized DataFrame
+
+print(df[['Age', 'Salary']].head())
+
+Scaler = StandardScaler()
+
+# apply normalization
+
+df[['Age', 'Salary']] = Scaler.fit_transform(df[['Age', 'Salary']])
+
+# show normalized DataFrame
+
+print("After Standard Scaling:")
+
+print(df[['Age', 'Salary']].head())
+
+# encode 'gender' column using label encoding
+
+le=LabelEncoder()
+
+df['Gender_encoded'] = le.fit_transform(df['Gender'])
+
+print("Display the label encoded 'Gender' column:")
+
+print(df[['Gender','Gender_encoded']].head())
+
+# onehot encode 
+
+df=pd.get_dummies(df, drop_first=True)
+
+# display update data frame
+
+print(df.head())
