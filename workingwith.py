88a4658893ed65e752b6b25fe1fbc7a31008259d
Naeem847 <ashoornaeem@gmail.com>
2026-07-21 16:52:33 +0500

    in this codin this code we explored the different csv files like how to working the csv files ,opening the csv files from an url ,tsv files separete parameters s it right sentence.e eplored

---
+++ workingwith.py
@@ -0,0 +1,21 @@
+
+import pandas as pd
+df=pd.read_csv('myenv/sample_submission.tsv',sep='\t')
+print(df)
+
+# opening a CSV file from an url
+
+import requests
+from io import StringIO
+
+url="D:\object_oriented_programming1.py\ML-Practice-code\myenv\sample_submission.csv"
+
+req=requests.get(url)
+data=StringIO(req.text)
+df=pd.read_csv(data)
+print(df)
+
+#  seperate paramenters
+import pandas as pd
+df=pd.read_csv('myenv/sample_submission.tsv',sep='\t')
+print(df)
