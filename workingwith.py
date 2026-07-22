e5d2099077509a26c32e270d2f99b24a1e85cc64
Naeem847 <ashoornaeem@gmail.com>
2026-07-22 17:38:10 +0500

    today we explored the how to  handle the differ issues of csv files like change print the specific columns,change the dtyes an soon.

--- workingwith.py
+++ workingwith.py
@@ -1,9 +1,9 @@
 
 import pandas as pd
-df=pd.read_csv('myenv/sample_submission.tsv',sep='\t')
+df=pd.read_csv('D:\object_oriented_programming1.py\ML-Practice-code\myenv\sample_submissio.csv')
 print(df)
 
-# opening a CSV file from an url
+# # opening a CSV file from an url
 
 import requests
 from io import StringIO
@@ -20,3 +20,38 @@ import pandas as pd
 df=pd.read_csv('myenv/sample_submission.tsv',sep='\t')
 print(df)
 
+# index__col parameter 
+df=pd.read_csv('D:\object_oriented_programming1.py\ML-Practice-code\myenv\sample_submissio.csv',index_col='EmployeeID',header=1)
+print(pd)
+
+# use cols__parameters in ML not necessary you will use all that necessart columns:
+
+df=pd.read_csv('D:\object_oriented_programming1.py\ML-Practice-code\myenv\sample_submissio.csv',usecols=['Name','Gender','Education'])
+print(df)          
+
+# squaze parameters: if we execute this statement then return pandas series not return the data frame of pandas :
+df=pd.read_csv('D:\object_oriented_programming1.py\ML-Practice-code\myenv\sample_submissio.csv',usecols=['Name'],squeeze=True)
+print(df)  
+
+# # skip/nrows parameters
+df=pd.read_csv('D:\object_oriented_programming1.py\ML-Practice-code\myenv\sample_submissio.csv',skiprows=[1,2,3])
+print(df)
+
+# skip/nrows parameters
+df=pd.read_csv('D:\object_oriented_programming1.py\ML-Practice-code\myenv\sample_submissio.csv',nrows=3)
+print(df)
+
+# df=pd.read_csv('D:\object_oriented_programming1.py\ML-Practice-code\myenv\sample_submissio.csv',nrows=3)
+# print(df)
+
+# Encoding paremeters in this statement in any csv file not working properly then use ths statement
+# in this code any mistakes in any lines then that mistakes will be skip.then output will display
+df=pd.read_csv('zomato.csv',encoding='latin-1',error_bad_lines=False)
+print(df)
+
+# dtypes parameters
+df=pd.read_csv('D:\object_oriented_programming1.py\ML-Practice-code\myenv\sample_submissio.csv').info()
+
+# if we need spacific column convert into integer
+
+df=pd.read_csv('D:\object_oriented_programming1.py\ML-Practice-code\myenv\sample_submissio.csv',dtype={'salary':float})
